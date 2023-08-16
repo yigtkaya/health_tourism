652aaffda79a2aee032376a3bd8df4632287323a
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-16 20:16:42 +0300

    images and packages view done

--- lib/view/clinics/clinic_detail_view.dart
+++ lib/view/clinics/clinic_detail_view.dart
@@ -1,14 +1,17 @@
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:health_tourism/core/constants/vertical_space.dart';
 import 'package:path/path.dart';
+import 'package:readmore/readmore.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../core/components/ht_icon.dart';
 import '../../core/components/ht_text.dart';
 import '../../core/constants/asset.dart';
 import '../../core/constants/horizontal_space.dart';
+import '../../product/navigation/route_paths.dart';
 import '../../product/theme/styles.dart';
 
 class ClinicDetailView extends StatefulWidget {
@@ -30,95 +33,197 @@ class _ClinicDetailViewState extends State<ClinicDetailView> {
   @override
   Widget build(BuildContext context) {
     final controller = CarouselController();
+    final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              CarouselSlider.builder(
-                  carouselController: controller,
-                  itemCount: clinicImages.length,
-                  itemBuilder: (context, index, realIndex) {
-                    final urlImage = clinicImages[index];
-                    return buildImage(urlImage, index);
-                  },
-                  options: CarouselOptions(
-                      height: 400,
-                      autoPlay: true,
-                      enableInfiniteScroll: false,
-                      autoPlayAnimationDuration: Duration(seconds: 2),
-                      viewportFraction: 1,
-                      enlargeCenterPage: false,
-                      onPageChanged: (index, reason) =>
-                          setState(() => activeIndex = index))),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: buildIndicator(),
-              ),
-            ],
-          ),
-          const VerticalSpace(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomCenter,
               children: [
-                Row(
-                  children: [
-                    HTText(label: "Vera Clinic", style: htTitleStyle),
-                    const Spacer(),
-                    HTIcon(
-                      iconName: AssetConstants.icons.star,
-                      width: 20,
-                      height: 20,
-                    ),
-                    const HorizontalSpace(
-                      spaceAmount: 3,
-                    ),
-                    HTText(label: "4.5", style: htBlueLabelStyle),
-                    const HorizontalSpace(
-                      spaceAmount: 6,
-                    ),
-                  ],
+                CarouselSlider.builder(
+                    carouselController: controller,
+                    itemCount: clinicImages.length,
+                    itemBuilder: (context, index, realIndex) {
+                      final urlImage = clinicImages[index];
+                      return buildImage(urlImage, index, context);
+                    },
+                    options: CarouselOptions(
+                        height: 400,
+                        autoPlay: true,
+                        enableInfiniteScroll: false,
+                        autoPlayAnimationDuration: Duration(seconds: 2),
+                        viewportFraction: 1,
+                        enlargeCenterPage: false,
+                        onPageChanged: (index, reason) =>
+                            setState(() => activeIndex = index))),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: buildIndicator(),
                 ),
-                const VerticalSpace(),
-                HTText(label: "Istanbul, Türkiye", style: htBlueLabelStyle),
               ],
             ),
-          ),
-          const VerticalSpace(),
-          const Divider(
-            height: 1,
-            thickness: 2,
-            color: Color(0x33000000),
-          ),
-          const VerticalSpace(),
-          buildInformation(),
-        ],
+            const VerticalSpace(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      HTText(label: "Vera Clinic", style: htTitleStyle),
+                      const Spacer(),
+                      HTIcon(
+                        iconName: AssetConstants.icons.star,
+                        width: 20,
+                        height: 20,
+                      ),
+                      const HorizontalSpace(
+                        spaceAmount: 3,
+                      ),
+                      HTText(label: "4.5", style: htBlueLabelStyle),
+                      const HorizontalSpace(
+                        spaceAmount: 6,
+                      ),
+                    ],
+                  ),
+                  const VerticalSpace(),
+                  HTText(label: "Istanbul, Türkiye", style: htBlueLabelStyle),
+                ],
+              ),
+            ),
+            const VerticalSpace(),
+            const Divider(
+              height: 1,
+              thickness: 2,
+              color: Color(0x33000000),
+            ),
+            const VerticalSpace(),
+            buildInformation(size),
+          ],
+        ),
       )),
     );
   }
 
-  Widget buildInformation() {
+  Widget buildInformation(Size size) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-        HTText(label: "About", style: htTitleStyle),
-        const VerticalSpace(),
-        const HTText(
-            label: "placehoplaceholderplac"
-                "eholderplaceholderplaceholderplaceholderplaceho"
-                "ülderplaceholderplaceholderplaceholderlderplaceholderpl"
-                "aceholderplaceholderplaceholder",
-            style: htLabelBlackStyle)
-      ]),
+        children: [
+          HTText(label: "About", style: htDarkBlueLargeStyle),
+          const VerticalSpace(),
+          const ReadMoreText(
+              "placehoplaceholderplac"
+              "eholderplaceholderplaceholderplaceholderplaceho"
+              "ülderplaceholderplaceholderplaceholderlderplaceholderpl"
+              "aceholderplaceholderplaceholderaceholderplaceholderplaceholdera"
+              "ceholaceholderplaceholderplaceholderaceholderplaceholderplacehold"
+              "erderaceholderplaceholderplaceholderaceholderplaceholderplaceholderpl"
+              "acehoaceholderplaceholderplaceholderaceholderplaceholderplaceholderlderplacehol"
+              "deraceholderplaceholderplaceholderaceholderplaceholderplaceholder",
+              style: htLabelBlackStyle,
+              trimLines: 4,
+              colorClickableText: Colors.blue,
+              trimMode: TrimMode.Line,
+              trimCollapsedText: 'Read more',
+              trimExpandedText: ' show less'),
+          const VerticalSpace(),
+          HTText(label: "Operations", style: htDarkBlueLargeStyle),
+          const VerticalSpace(),
+          Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ImageHolder(size),
+                ImageHolder(size),
+                ImageHolder(size),
+              ],
+            ),
+          ),
+          const VerticalSpace(),
+          HTText(label: "Packages", style: htDarkBlueLargeStyle),
+          const VerticalSpace(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                createPackages(size),
+                const HorizontalSpace(),
+                createPackages(size),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+
+  Widget createPackages(Size size) {
+    return GestureDetector(
+      onTap: () {
+        /// TODO: Navigate to package detail page ??
+      },
+      child: Container(
+        width: size.width * 0.43,
+        decoration: const BoxDecoration(
+          color: Color(0xffd3e9ff),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Center(
+                  child:
+                      HTText(label: "PackageTitle", style: htDarkBlueLargeStyle)),
+            ),
+            const Divider(
+              height: 1,
+              thickness: 1,
+              color: Colors.black,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                   HTText(
+                      label: "- Maximum Graft", style: htLabelBlackStyle),
+                   HTText(
+                      label: "- Gives 100% satisfaction guarantee",
+                      style: htLabelBlackStyle),
+                   HTText(
+                      label: "- 2 Nights stay in the Hotel",
+                      style: htLabelBlackStyle),
+                   HTText(label: "- Checkup", style: htLabelBlackStyle),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget ImageHolder(Size size) => Container(
+        height: size.height * 0.1,
+        width: size.width * 0.25,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          image: const DecorationImage(
+              image: NetworkImage(
+                  "https://healthwaymedical.com/wp-content/uploads/2022/01/Medico-Clinic-Surgery-1024x681.jpg"),
+              fit: BoxFit.cover),
+        ),
+      );
 
   Widget buildIndicator() => AnimatedSmoothIndicator(
         activeIndex: activeIndex,
@@ -132,11 +237,18 @@ class _ClinicDetailViewState extends State<ClinicDetailView> {
             dotColor: Colors.grey),
       );
 
-  Widget buildImage(String urlImage, int index) => ClipRRect(
+  Widget buildImage(String urlImage, int index, BuildContext context) =>
+      ClipRRect(
         borderRadius: const BorderRadius.only(
           bottomRight: Radius.circular(22),
           bottomLeft: Radius.circular(22),
         ),
-        child: Image.network(urlImage, fit: BoxFit.cover),
+        child: GestureDetector(
+            onTap: () {
+              context.pushNamed(RoutePath.fullscreenImage, queryParameters: {
+                "imageUrl": urlImage,
+              });
+            },
+            child: Image.network(urlImage, fit: BoxFit.cover)),
       );
 }
