62856088aebd783703a145c21f12a7c6111c4181
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-18 09:31:59 +0300

    nottom nav bar fixed

--- lib/view/bottom_navigation/nav_bar_dot.dart
+++ lib/view/bottom_navigation/nav_bar_dot.dart
@@ -41,38 +41,54 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                GestureDetector(
-                  onTap: () {
-                    widget.onChange(0);
-                    setState(() {
-                      _selectedIndex = 0;
-                    });
-                  },
-                  child: _selectedIndex == 0
-                      ? HTIcon(iconName: AssetConstants.icons.homeSelected, width: 24, height: 24,)
-                      : HTIcon(iconName: AssetConstants.icons.homeUnSelected, width: 24, height: 24,),
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      widget.onChange(0);
+                      setState(() {
+                        _selectedIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 0
+                          ? HTIcon(iconName: AssetConstants.icons.homeSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.homeUnSelected, width: 24, height: 24,),
+                    ),
+                  ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    widget.onChange(1);
-                    setState(() {
-                      _selectedIndex = 1;
-                    });
-                  },
-                  child: _selectedIndex == 1
-                      ? HTIcon(iconName: AssetConstants.icons.messageSelected, width: 24, height: 24,)
-                      : HTIcon(iconName: AssetConstants.icons.messageUnSelected, width: 24, height: 24,),
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+
+                    onTap: () {
+                      widget.onChange(1);
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 1
+                          ? HTIcon(iconName: AssetConstants.icons.messageSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.messageUnSelected, width: 24, height: 24,),
+                    ),
+                  ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    widget.onChange(2);
-                    setState(() {
-                      _selectedIndex = 2;
-                    });
-                  },
-                  child: _selectedIndex == 2
-                      ? HTIcon(iconName: AssetConstants.icons.profileSelected, width: 24, height: 24,)
-                      : HTIcon(iconName: AssetConstants.icons.profileUnSelected, width: 24, height: 24,),
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      widget.onChange(2);
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 2
+                          ? HTIcon(iconName: AssetConstants.icons.profileSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.profileUnSelected, width: 24, height: 24,),
+                    ),
+                  ),
                 ),
               ],
             ),
