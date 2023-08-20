01c4bdb951469478947e04f9cabab6a7b01de32a
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-20 21:41:31 +0300

    Update package.dart

--- lib/product/models/package.dart
+++ lib/product/models/package.dart
@@ -1,13 +1,25 @@
 class Package {
-  final String id;
-  final String name;
+  final String packageName;
   final double price;
-  final String category;
+  final List<dynamic> packageFeatures;
 
   Package({
-    required this.id,
-    required this.name,
+    required this.packageName,
     required this.price,
-    required this.category,
+    required this.packageFeatures,
   });
+
+  List<Object> get props => [
+        packageName,
+        price,
+        packageFeatures,
+      ];
+
+  bool get stringify => true;
+
+  Package.fromData(Map<dynamic, dynamic> data)
+      : packageName = data['packageName'],
+        price = data['packagePrice'].toDouble(),
+        packageFeatures = data['packageFeatures'];
+
 }
