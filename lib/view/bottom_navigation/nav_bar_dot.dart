f0f31deaf10e2962d2b128f0b6903e09bd38f3f0
Hasan Yiğit Kaya <68725704+yigtkaya@users.noreply.github.com>
2023-08-20 03:03:32 +0300

    bottom nav bar fix

--- lib/view/bottom_navigation/nav_bar_dot.dart
+++ lib/view/bottom_navigation/nav_bar_dot.dart
@@ -27,75 +27,73 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Container(
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(3), topRight: Radius.circular(3))),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {
-                        widget.onChange(0);
-                        setState(() {
-                          _selectedIndex = 0;
-                        });
-                      },
-                      child: Container(
-                        child: _selectedIndex == 0
-                            ? HTIcon(iconName: AssetConstants.icons.homeSelected, width: 24, height: 24,)
-                            : HTIcon(iconName: AssetConstants.icons.homeUnSelected, width: 24, height: 24,),
-                      ),
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(3), topRight: Radius.circular(3))),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0, bottom: 32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      widget.onChange(0);
+                      setState(() {
+                        _selectedIndex = 0;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 0
+                          ? HTIcon(iconName: AssetConstants.icons.homeSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.homeUnSelected, width: 24, height: 24,),
                     ),
                   ),
-                  Expanded(
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
 
-                      onTap: () {
-                        widget.onChange(1);
-                        setState(() {
-                          _selectedIndex = 1;
-                        });
-                      },
-                      child: Container(
-                        child: _selectedIndex == 1
-                            ? HTIcon(iconName: AssetConstants.icons.messageSelected, width: 24, height: 24,)
-                            : HTIcon(iconName: AssetConstants.icons.messageUnSelected, width: 24, height: 24,),
-                      ),
+                    onTap: () {
+                      widget.onChange(1);
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 1
+                          ? HTIcon(iconName: AssetConstants.icons.messageSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.messageUnSelected, width: 24, height: 24,),
                     ),
                   ),
-                  Expanded(
-                    child: GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {
-                        widget.onChange(2);
-                        setState(() {
-                          _selectedIndex = 2;
-                        });
-                      },
-                      child: Container(
-                        child: _selectedIndex == 2
-                            ? HTIcon(iconName: AssetConstants.icons.profileSelected, width: 24, height: 24,)
-                            : HTIcon(iconName: AssetConstants.icons.profileUnSelected, width: 24, height: 24,),
-                      ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      widget.onChange(2);
+                      setState(() {
+                        _selectedIndex = 2;
+                      });
+                    },
+                    child: Container(
+                      child: _selectedIndex == 2
+                          ? HTIcon(iconName: AssetConstants.icons.profileSelected, width: 24, height: 24,)
+                          : HTIcon(iconName: AssetConstants.icons.profileUnSelected, width: 24, height: 24,),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
